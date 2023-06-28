c367a0a096c840bde38d42cf612e0937a1052930
sri <85162129+sri5952@users.noreply.github.com>
2023-06-28 15:55:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "aws_region" {
 
 variable "ecs_task_execution_role_name" {
   description = "ECS task execution role name"
-  default     = "ECSTaskExecutionRole"
+  default     = "lessonsmgmtECSTaskExecutionRole"
 }
 
 variable "az_count" {
