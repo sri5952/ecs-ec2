b7b1515cd52f93faa1622ff7cfba5f8b424c560c
sri <85162129+sri5952@users.noreply.github.com>
2023-06-28 15:59:10 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -79,15 +79,15 @@ resource "aws_launch_configuration" "lessonmgmt" {
 }
 
 
-resource "aws_autoscaling_group" "bar" {
-  name                      = "launchconfig"
-  max_size                  = 3
-  min_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 2
-  force_delete              = true
-  launch_configuration      = aws_launch_configuration.lessonmgmt.name
-  vpc_zone_identifier       = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+    name                      = "asg"
+    vpc_zone_identifier       = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    launch_configuration      = aws_launch_configuration.lessonmgmt.name
+    desired_capacity          = 2
+    min_size                  = 1
+    max_size                  = 4
+    health_check_grace_period = 300
+    health_check_type         = "EC2"
 }
+
 
