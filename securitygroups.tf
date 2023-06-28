3540d5a1a74e783d96702e8401905929353598d3
sri <85162129+sri5952@users.noreply.github.com>
2023-06-28 18:28:23 +0530

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "lessonsmgmt" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
