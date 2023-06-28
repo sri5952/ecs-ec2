69e0a75b6dc6743fe5fe8e0b43aa5254c5a649e8
sri <85162129+sri5952@users.noreply.github.com>
2023-06-28 18:35:48 +0530

    Update userdata.sh

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo amazon-linux-extras disable docker
-sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
 echo  ECS_CLUSTER="lessons-mgmt-cluster">> /etc/ecs/ecs.config
+sudo yum install -y ecs-init
+sudo service docker start
+sudo service ecs start
+
