9bdafaae22e1b22f9d0ec2b62500ea7d793db189
sri <85162129+sri5952@users.noreply.github.com>
2023-06-28 18:39:25 +0530

    Update userdata.sh

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -3,4 +3,4 @@ echo  ECS_CLUSTER="lessons-mgmt-cluster">> /etc/ecs/ecs.config
 sudo yum install -y ecs-init
 sudo service docker start
 sudo service ecs start
-
+systemctl enable --now --no-block ecs.service
